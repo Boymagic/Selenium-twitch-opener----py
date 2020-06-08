8d26109b270f399e484c9b4239f42ff8f369453b
Joao Lucas Camilo <jlf.camilo@icloud.com>
2020-06-07 23:27:37 -0400

    First release commit

---
+++ support_your_friend_twitch_channel.py
@@ -0,0 +1,18 @@
+#Import section
+from selenium import webdriver
+
+#Setup config
+PATH = "chromedriver.exe"
+driver = webdriver.Chrome(self.PATH)
+driver.maximize_window()
+
+# Support your friends
+driver.get('https://www.twitch.tv/madyfilho')
+
+driver.get('https://www.twitch.tv/x_tefa')
+
+driver.get('https://www.twitch.tv/thiagoskm')
+
+driver.get('https://www.twitch.tv/boydesue')
+
+    
